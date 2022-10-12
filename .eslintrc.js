012803e92523f0f7b7f3129a4d2cd955e1c9e8ee
Tommy <47206288+tommysr@users.noreply.github.com>
2022-10-12 17:12:43 +0200

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: ['standard-with-typescript'],
+  parserOptions: {
+    project: './tsconfig.json'
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['prettier'],
+  rules: {
+    indent: 'off',
+    '@typescript-eslint/indent': 'off',
+    '@typescript-eslint/space-before-function-paren': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/restrict-plus-operands': 'off',
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    '@typescript-eslint/promise-function-async': 'off',
+    '@typescript-eslint/prefer-nullish-coalescing': 'off',
+    'spaced-comment': 'off',
+    'prettier/prettier': ['error']
+  }
+}
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import { greet } from 'sdk'
+
+greet('world')
